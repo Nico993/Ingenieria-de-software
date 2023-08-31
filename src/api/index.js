0860c66627d7283c4b58d5a313c07af3064bbcf9
Agostina Bertoni <abertoni@facultad.sanfrancisco.utn.edu.ar>
2023-08-30 22:17:26 -0300

    Agregar producto

--- src/api/index.js
+++ src/api/index.js
@@ -1,1 +1,24 @@
-const URL = "http://localhost:7000/api/";
+import axios from "axios"
+const URL = "http://vps.simix.tech:9191/v1/reconocimiento-productos/";
+
+function nuevoProducto(formData){
+
+    return new Promise((resolve,reject)=>{
+        axios.post(URL+'productos',formData)
+        .then(response=>{
+            if(response.status===201){
+                resolve(response.data);
+            }
+        })
+        .catch(error=>{
+            if(error.response){
+                reject(error.response.data);
+            } else{
+                reject(error);
+            }
+        });
+    });
+
+}
+
+export{nuevoProducto}
--- src/components/Home/AgregarProductoPopUp.jsx
+++ src/components/Home/AgregarProductoPopUp.jsx
@@ -1,28 +1,73 @@
 import styles from "../../styles/agregarProductos.module.css"
-import React from 'react'
+import React, { useState } from 'react'
+import { nuevoProducto } from "../../api";
 
 export const AgregarProductoPopUp = (props) => {
-    function handleSubmit(e){
-        e.preventDefault()
+    const [formData, setFormData]=useState({
+        nombre: '',
+        precio: 0,
+        cantidad: 0,
+        descripcion: '',
+        imagen: null,
+    });
+
+    const handleInputChange=(event)=>{
+        const{name, value}=event.target;
+        setFormData((prevData)=>({
+            ...prevData,
+            [name]:value,
+        }));
     }
+
+    const handleImageChange = (event) => {
+        const imageFile = event.target.files[0];
+        setFormData((prevData)=>({
+            ...prevData,
+            imagen: imageFile,
+        }));
+    }
+
+    function handleSubmit(e){
+        e.preventDefault();
+        const form=new FormData();
+        const productoJson={
+            nombre:formData.nombre,
+            precio_unitario:formData.precio,
+            cantidad:formData.cantidad,
+            descripcion:formData.descripcion,
+        };
+        const jsonString=JSON.stringify(productoJson);
+        const jsonBlob=new Blob ([jsonString],{type:'application/json'});
+
+        form.append("producto",jsonBlob,"producto.json");
+        form.append("imagen",formData.imagen)
+
+        nuevoProducto(form).then((res)=>{
+            console.log(res)
+        })
+        .catch((err)=>{
+            console.log(err)
+        })
+    }
+
 
   return (
     <div className={styles.main}>
-        <form onSubmit={handleSubmit}>
+        <form onSubmit={handleSubmit} >
             <div className={styles.formulario}>
                 <div className={styles.imagen}>
                     <label htmlFor="upload-input" className={styles.uploadLabel}>
                         <div className={styles.uploadIcon}>+</div>
                         <div>Agregar imagen</div>
                     </label>
-                    <input id="upload-input" type="file" className={styles.uploadInput} />
+                    <input id="upload-input" type="file" className={styles.uploadInput} onChange={handleImageChange} required/>
                 </div>
                 <div className={styles.detalle}>
                     <h4 className={styles.titulo}>Nuevo producto</h4>
-                    <input type="text" placeholder='Nombre' className={styles.input}></input>
-                    <input type="number" placeholder='Precio' className={styles.input}></input>
-                    <input type="number" placeholder='Cantidad' className={styles.input}></input>
-                    <input type="text" placeholder='Descripcion' className={styles.input}></input>
+                    <input type="text" placeholder='Nombre' className={styles.input} name='nombre' onChange={handleInputChange} required></input>
+                    <input type="number" placeholder='Precio' className={styles.input} name='precio' onChange={handleInputChange} required></input>
+                    <input type="number" placeholder='Cantidad' className={styles.input} name='cantidad' onChange={handleInputChange} required></input>
+                    <input type="text" placeholder='Descripcion' className={styles.input} name='descripcion' onChange={handleInputChange} required></input>
                     <div>
                         <button type="submit">Aceptar</button>
                         <button type="button" onClick={props.close}>Cancelar</button>
